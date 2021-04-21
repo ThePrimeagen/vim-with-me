81a1943061217b8bc90f81aa94a351d0df5e9636
ThePrimeagen <mpaulson@netflix.com>
2021-04-20 20:31:11 -0600

    feat(test)

--- src/__tests__/cmd.ts
+++ src/__tests__/cmd.ts
@@ -7,6 +7,7 @@ const goldenBuffer = Buffer.from(
             13, 37,
         ]).concat(new Array(200).fill(23))
 );
+const empty = Buffer.alloc(50 + 1 + 2 + 200).fill(0);
 
 describe("Command", function() {
 
@@ -28,7 +29,15 @@ describe("Command", function() {
             setStatusLine(new Array(50).fill("E").join("")).
             setCost(13 * 256 + 37).
             setData(Buffer.alloc(200).fill(23)).reset();
+        expect(cmd.buffer).toEqual(empty);
     });
 
+    it("should set the type", function() {
+        const cmd = new Command();
+        cmd.
+            setType(CommandType.StatusUpdate);
+
+        expect(cmd.buffer[0]).toEqual(CommandType.StatusUpdate);
+    });
 });
 
