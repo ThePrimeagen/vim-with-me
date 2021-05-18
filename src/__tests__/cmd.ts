43374283270cca9bfbf59be1720e3936c1f57052
ThePrimeagen <mpaulson@netflix.com>
2021-05-18 09:03:57 -0600

    fix(test): Forgot I renamed ASDF to SystemCommand

--- src/__tests__/cmd.ts
+++ src/__tests__/cmd.ts
@@ -2,7 +2,7 @@ import Command, { CommandType } from "../cmd";
 
 const goldenBuffer = Buffer.from(
     //@ts-ignore
-    [CommandType.ASDF].concat(
+    [CommandType.SystemCommand].concat(
         new Array(50).fill(69)).concat([
             13, 37,
         ]).concat(new Array(200).fill(23))
@@ -14,7 +14,7 @@ describe("Command", function() {
     it("should be able to set in some values.", function() {
         const cmd = new Command();
         cmd.
-            setType(CommandType.ASDF).
+            setType(CommandType.SystemCommand).
             setStatusLine(new Array(50).fill("E").join("")).
             setCost(13 * 256 + 37).
             setData(Buffer.alloc(200).fill(23));
@@ -25,7 +25,7 @@ describe("Command", function() {
     it("should reset the buffer back to 0.", function() {
         const cmd = new Command();
         cmd.
-            setType(CommandType.ASDF).
+            setType(CommandType.SystemCommand).
             setStatusLine(new Array(50).fill("E").join("")).
             setCost(13 * 256 + 37).
             setData(Buffer.alloc(200).fill(23)).reset();
