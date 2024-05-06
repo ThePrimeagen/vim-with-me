9333daf23d3a4ff00068918c1ccebe9b17c25ede
theprimeagen <theprimeagen@gmail.com>
2024-05-06 01:13:01 -0600

    fix: format

--- pkg/v2/ascii_buffer/frequency.go
+++ pkg/v2/ascii_buffer/frequency.go
@@ -1,23 +1,23 @@
 package ascii_buffer
 
 type Frequency struct {
-    Buffer [256]int
+	Buffer [256]int
 }
 
 func NewFreqency() Frequency {
-    return Frequency{
-        Buffer: [256]int{},
-    }
+	return Frequency{
+		Buffer: [256]int{},
+	}
 }
 
 func (f *Frequency) Reset() {
-    for i := 0; i < 256; i++ {
-        f.Buffer[i] = 0
-    }
+	for i := 0; i < 256; i++ {
+		f.Buffer[i] = 0
+	}
 }
 
 func (f *Frequency) Freq(data []byte) {
-    for _, b := range data {
-        f.Buffer[b]++
-    }
+	for _, b := range data {
+		f.Buffer[b]++
+	}
 }
