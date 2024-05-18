d454e80bec76d25b468ce096f1d29cc90271807c
theprimeagen <theprimeagen@gmail.com>
2024-05-17 18:21:21 -0600

    feat: client relay driver

---
+++ pkg/v2/relay/client.go
@@ -0,0 +1,39 @@
+package relay
+
+import (
+	"net/url"
+
+	"github.com/gorilla/websocket"
+	"github.com/theprimeagen/vim-with-me/pkg/v2/assert"
+)
+
+type RelayDriver struct {
+	url url.URL
+    uuid string
+    conn *websocket.Conn
+}
+
+func NewRelayDriver(addr string, uuid string) *RelayDriver {
+
+    u := url.URL{Scheme: "ws", Host: addr, Path: "/"}
+    return &RelayDriver{
+        url: u,
+        uuid: uuid,
+    }
+}
+
+func (r *RelayDriver) Connect() error {
+    assert.Assert(r.conn == nil, "attempting to connect while connected")
+
+	c, _, err := websocket.DefaultDialer.Dial(r.url.String(), nil)
+    if err != nil {
+        return err
+    }
+
+    r.conn = c
+    return c.WriteMessage(websocket.BinaryMessage, []byte(r.uuid))
+}
+
+func (r *RelayDriver) Relay(data []byte) error {
+    return r.conn.WriteMessage(websocket.BinaryMessage, data)
+}
