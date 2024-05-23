f53fe184a089525a69876b46345c38a9f4865296
Stuart Dallas <stuart@stut.net>
2024-05-23 22:08:12 +0100

    Added an option to write a metrics file

--- pkg/v2/relay/cmd/server/main.go
+++ pkg/v2/relay/cmd/server/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log/slog"
 	"os"
 	"strconv"
 	"sync"
+	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/joho/godotenv"
@@ -24,6 +26,9 @@ func main() {
 
 	var port uint = 0
 	flag.UintVar(&port, "port", 0, "the port to run on for the websocket")
+	var staticMetricsFilename string
+	flag.StringVar(&staticMetricsFilename, "metrics-filename", "", "a filename to write the metrics to (plain text)")
+	flag.Parse()
 
 	if port == 0 {
 		portStr := os.Getenv("PORT")
@@ -31,6 +36,10 @@ func main() {
 		if err == nil {
 			port = uint(portEnv)
 		}
+	}
+
+	if staticMetricsFilename == "" {
+	    staticMetricsFilename = os.Getenv("METRICS_FILENAME")
 	}
 
 	assert.Assert(port != 0, "please provide a port for the relay server")
@@ -48,6 +57,23 @@ func main() {
 
 	go newConnections(r, connMsgs)
 	go onMessage(r, connMsgs)
+
+	if staticMetricsFilename != "" {
+		ticker := time.NewTicker(10 * time.Second)
+		go func() {
+			for {
+				<-ticker.C
+				current, maxConcurrent, total := r.GetConnectionStats()
+				os.WriteFile(staticMetricsFilename,
+					[]byte(fmt.Sprintf(
+						"Current: %d\n" +
+						"Max concurrent: %d\n" +
+						"Total connections: %d",
+						current, maxConcurrent, total)),
+					0644)
+			}
+		}()
+	}
 
 	r.Start()
 }
