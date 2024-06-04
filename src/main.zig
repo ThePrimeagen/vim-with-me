6108a913ab0648e6dd391c946007e62806223757
theprimeagen <theprimeagen@gmail.com>
2024-06-04 10:06:52 -0600

    feat: i am going to try another form of this polymorphism...

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const print = std.debug.print;
 
-const render = @import("render/render.zig");
+const render = @import("engine/render.zig");
 const encoding = @import("encoding/encoding.zig");
 
 pub fn main() !void {
