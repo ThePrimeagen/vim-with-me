06007f717c6e7d4ed39652fca8f21896b9a3c718
theprimeagen <theprimeagen@gmail.com>
2024-06-09 14:01:19 -0600

    time added to engine

--- src/engine/engine.zig
+++ src/engine/engine.zig
@@ -60,7 +60,36 @@ pub const Engine = struct {
         }
     }
 
-    pub fn gameLoop(self: *Engine) void {
+    pub fn gameLoop(self: *Engine, timePassed: i64) void {
+        _ = timePassed;
         _ = self;
     }
+};
+
+const milliTimestamp = std.time.milliTimestamp();
+pub const RealTime = struct {
+    lastTime: i64,
+
+    pub fn reset(self: *RealTime) void {
+        const time = milliTimestamp();
+        self.lastTime = time;
+    }
+
+    pub fn tick(self: *RealTime) i64 {
+        const time = milliTimestamp();
+        const delta = time - self.lastTime;
+        self.lastTime = time;
+
+        return delta;
+    }
+
+};
+
+pub const FauxTime = struct {
+    returnDelta: i64,
+
+    pub fn tick(self: *FauxTime) i64 {
+        return self.returnDelta;
+    }
+
 };
