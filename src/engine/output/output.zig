09d67445055619a1760930b1777043b7ff316d91
theprimeagen <theprimeagen@gmail.com>
2024-06-09 14:01:45 -0600

    output basics

--- src/engine/output/output.zig
+++ src/engine/output/output.zig
@@ -10,25 +10,55 @@ const clear: [4]u8 = .{
     '', '[', ';', 'H',
 };
 
+const newline: [2]u8 = .{
+    '\n', '\r',
+};
+
+const ColorOutput = struct {
+    text: []u8,
+    r: u8,
+    g: u8,
+    b: u8,
+};
+
 pub const Output = struct {
     firstPrint: bool,
     rows: usize,
     cols: usize,
-    fn init(rows: usize, cols: usize) Output {
-        return .{
-            .firstPrint = true,
-            .rows = rows,
-            .cols = cols,
-        };
-    }
 
-    fn frame(self: *Output, f: []u8) !void {
+    pub fn frame(self: *Output, f: []ColorOutput) !void {
         assert(f.len == self.rows * self.cols, "you must hand in a frame that matches rows and cols");
+        const stdout = io.getStdOut().writer();
+        var buffered = io.bufferedWriter(stdout);
+        var writer = buffered.writer();
+
         if (self.firstPrint) {
+            try writer.write(initialClear);
         }
+
+        var charCount = 0;
+        for (f) |*c| {
+            const text = c.text;
+
+            for (text) |t| {
+                try writer.writeByte(t);
+                charCount += 1;
+
+                if (charCount % self.cols == 0) {
+                    try writer.write(newline);
+                }
+            }
+        }
+
+        assert(charCount == self.rows * self.cols, "did not produce the correct amount of characters");
+        try buffered.flush();
     }
 };
 
-pub fn initScreen() !void {
-    io.getStdOut().writer().print("{s}", initialClear);
+pub fn init(rows: usize, cols: usize) Output {
+    return .{
+        .firstPrint = true,
+        .rows = rows,
+        .cols = cols,
+    };
 }
