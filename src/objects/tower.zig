cadf475a201743636d3b2ef16a0a3a9eda62ffdf
theprimeagen <theprimeagen@gmail.com>
2024-06-23 22:26:14 -0600

    basic tower refactor completed.

--- src/objects/tower.zig
+++ src/objects/tower.zig
@@ -1,17 +1,7 @@
 const math = @import("math");
-
 const colors = @import("colors.zig");
 
-const Cell = colors.Cell;
-const Black = colors.Black;
-
 const INITIAL_AMMO = 50;
-const TowerSize = 3;
-const TowerCell: [3]Cell = .{
-    .{.text = '/', .color = Black },
-    .{.text = '*', .color = Black },
-    .{.text = '\\', .color = Black },
-};
 
 pub const Tower = struct {
     id: usize,
@@ -29,64 +19,7 @@ pub const Tower = struct {
     // rendered
     rSized: math.Sized = math.ZERO_SIZED,
     rAmmo: u16 = INITIAL_AMMO,
-    rCells: [3]Cell = TowerCell,
-
-    pub fn contains(self: *Tower, pos: math.Position) bool {
-        if (self.alive) {
-            return false;
-        }
-
-        const c = math.absUsize(self.pos.col, pos.col);
-        return self.pos.row == pos.row and c <= 1;
-    }
-
-    pub fn color(self: *Tower, c: colors.Color) void {
-        for (0..self.rCells.len) |idx| {
-            self.rCells[idx].color = c;
-        }
-    }
-
-    pub fn create(id: usize, team: u8, pos: math.Position) Tower {
-        var p = pos;
-        if (p.col == 0) {
-            p.col = 1;
-        }
-
-        return .{
-            .id = id,
-            .team = team,
-            .pos = p,
-            .rSized = .{
-                .cols = TowerSize,
-                .pos = p
-            },
-        };
-    }
-
-    pub fn update(self: *Tower) void {
-        if (!self.alive) {
-            return;
-        }
-    }
-
-    pub fn render(self: *Tower) void {
-
-        const life = self.getLifePercent();
-        const sqLife = life * life;
-
-        self.rCells[1].text = '0' + self.level;
-        self.color(.{
-            .r = @intFromFloat(255.0 * life),
-            .b = @intFromFloat(255.0 * sqLife),
-            .g = @intFromFloat(255.0 * sqLife),
-        });
-    }
-
-    fn getLifePercent(self: *Tower) f64 {
-        const max: f64 = @floatFromInt(self.maxAmmo);
-        const ammo: f64 = @floatFromInt(self.ammo);
-        return ammo / max;
-    }
+    rCells: [3]colors.Cell,
 };
 
 
