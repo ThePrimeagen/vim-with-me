c6bb2af8a0123b2c4c8f992f5ab38df88188de91
theprimeagen <theprimeagen@gmail.com>
2024-06-26 20:34:45 -0600

    rendering area + text area now

--- src/engine/renderer.zig
+++ src/engine/renderer.zig
@@ -1,5 +1,8 @@
+const assert = @import("assert").assert;
 const std = @import("std");
 const objects = @import("objects");
+const scratchBuf = @import("scratch").scratchBuf;
+
 const gamestate = objects.gamestate;
 const Values = objects.Values;
 const canvas = @import("canvas.zig");
@@ -9,24 +12,39 @@ const creeps = @import("creep.zig");
 
 const Allocator = std.mem.Allocator;
 const GameState = gamestate.GameState;
+const TEXT_AREA_COLS = 30;
 
 pub const Renderer = struct {
     canvas: canvas.Canvas,
     output: []u8,
     count: u32,
     values: *const objects.Values,
+    textOffset: usize,
+    rendererValues: *objects.Values,
+    alloc: Allocator,
 
     pub fn init(alloc: Allocator, values: *const Values) !Renderer {
+        assert(values.cols > 30, "not enough columns for game information.  requires 30 cols for text");
+
+        var rendererValues: *Values = try alloc.create(Values);
+        Values.copyInto(values, rendererValues);
+        rendererValues.cols = values.cols + TEXT_AREA_COLS;
+        Values.init(rendererValues);
+
         return .{
+            .textOffset = values.cols,
             .values = values,
-            .canvas = try canvas.Canvas.init(alloc, values),
+            .canvas = try canvas.Canvas.init(alloc, rendererValues),
             .output = undefined,
             .count = 0,
+            .rendererValues = rendererValues,
+            .alloc = alloc,
         };
     }
 
     pub fn deinit(self: *Renderer) void {
         self.canvas.deinit();
+        self.alloc.destroy(self.rendererValues);
     }
 
     pub fn render(self: *Renderer, gs: *GameState) !void {
@@ -40,16 +58,25 @@ pub const Renderer = struct {
             self.canvas.place(c.rSized, &c.rCells);
         }
 
-        var buff: [15]u8 = undefined;
-        _ = try std.fmt.bufPrint(&buff, "renders: {}", .{self.count});
-
-        self.canvas.writeText(.{
-            .row = 0,
-            .col = 14,
-        }, &buff, .{.r = 255, .g = 255, .b = 255});
-
+        try self.text(gs);
         try self.canvas.render();
         self.output = self.canvas.renderBuffer;
         self.count += 1;
+    }
+
+    pub fn text(self: *Renderer, gs: *GameState) !void {
+        const buf = scratchBuf(50);
+
+        // round
+        const roundBuf = try std.fmt.bufPrint(buf, "round: {}", .{gs.round});
+
+        var row: usize = 0;
+
+        self.canvas.writeText(.{
+            .row = row,
+            .col = self.textOffset,
+        }, roundBuf, .{.r = 255, .g = 255, .b = 255});
+
+        row += 1;
     }
 };
