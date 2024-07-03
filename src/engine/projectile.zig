8b4f660730824ad32cb73e503fb1618530e6a447
theprimeagen <theprimeagen@gmail.com>
2024-07-02 21:05:01 -0600

    shooting

--- src/engine/projectile.zig
+++ src/engine/projectile.zig
@@ -6,3 +6,8 @@ pub fn update(self: *Projectile, gs: *GS) void {
     _ = self;
     _ = gs;
 }
+
+pub fn render(self: *Projectile, gs: *GS) void {
+    self.rSized.pos = self.pos.position();
+    _ = gs;
+}
